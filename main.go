2b51687bf8bced05fb0929c04ad1385483f493bc
John Nasman <john.nasman@iki.fi>
2015-11-20 18:34:13 +0200

    server address parameterized, trace module started

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -15,15 +16,19 @@ type templateHandler struct {
 	templ    *template.Template
 }
 
-// ServeHTTP handles the HTTP requet.
+// ServeHTTP handles the HTTP request.
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	// process command line parameters (flags)
+	var addr = flag.String("addr", ":8080", "The address of application")
+	flag.Parse()
+
 	r := newRoom()
 	// root
 	http.Handle("/", &templateHandler{filename: "chat.html"})
@@ -32,7 +37,8 @@ func main() {
 	go r.run()
 
 	//start the web server
-	err := http.ListenAndServe(":8080", nil)
+	log.Println("Starting Chat server on address: ", *addr)
+	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
